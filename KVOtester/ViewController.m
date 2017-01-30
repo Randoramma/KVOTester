c5428f680d61734c96edd8c58cd5ffa13bb392cd
Randy McLain <randy.mclain@ikegps.com>
2017-01-30 10:10:07 -0800

    Initial Commit

---
+++ KVOtester/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KVOtester
+//
+//  Created by Randy McLain on 1/30/17.
+//  Copyright © 2017 Randy McLain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
